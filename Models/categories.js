f3cc095d57b2fb99140fd56988ae760782f401c1
Malkibrahim <37645796+Malkibrahim@users.noreply.github.com>
2020-05-08 22:58:42 +0200

    connection edit

--- Models/categories.js
+++ Models/categories.js
@@ -1,8 +1,5 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/user", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
 var Schema = mongoose.Schema;
 var categorySchema = new Schema({
   name: {
