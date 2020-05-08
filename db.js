b087c8b5dbc6d734c6a26f86a7862649a646a9f5
Malkibrahim <37645796+Malkibrahim@users.noreply.github.com>
2020-05-09 00:06:33 +0200

    connectionn envvvv

--- db.js
+++ db.js
@@ -2,13 +2,10 @@ var mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(
-    "mongodb+srv://test123:112233445566@cluster0-6ego3.mongodb.net/users-app?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.CONNECT_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log(process.env.CONNECT_URI);
     console.log("hhhhhhhhhhhhhh");
